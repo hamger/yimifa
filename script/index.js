47b594f5f4e7c64191ca8a6d29bb0988610be30d
Hanger <2313303800@qq.com>
2016-12-28 17:57:52 +0800

    ymf

---
+++ script/index.js
@@ -0,0 +1,33 @@
+function login() {
+    var userName = $("#username").val();
+    var userPass = $("#password").val();
+    if (!userName) {
+        alert("用户名不能为空");
+    } else if (!userPass) {
+        alert("密码不能为空");
+
+    } else {
+        /*$('#login').addClass('hide');*/
+        $('#login').toggle();
+    }
+}
+
+function tab1() {
+    $('.tab').removeClass('show');
+    $('#tab1').addClass('show');
+}
+
+function tab2() {
+    $('.tab').removeClass('show');
+    $('#tab2').addClass('show');
+}
+
+function tab3() {
+    $('.tab').removeClass('show');
+    $('#tab3').addClass('show');
+}
+
+function tab4() {
+    $('.tab').removeClass('show');
+    $('#tab4').addClass('show');
+}
