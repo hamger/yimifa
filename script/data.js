3fb2a16b10d1a3e560216b2d918f25d4d119e3ac
Hanger <2313303800@qq.com>
2016-12-31 15:12:59 +0800

    enhance3

--- script/data.js
+++ script/data.js
@@ -122,12 +122,134 @@ var data = [{
         meaning: "腾讯域名",
         price: "0",
         type: "三字母"
+    }, {
+        admin_uid: "8",
+        bid_price: "3999元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "ykkf.com",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "四声"
+    },
+
+    {
+        admin_uid: "9",
+        bid_price: "21381元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "ydsd.cn",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "四字母"
+    },
+
+    {
+        admin_uid: "10",
+        bid_price: "8765元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "76599.com",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "三声"
+    },
+
+    {
+        admin_uid: "11",
+        bid_price: "5421元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "27655.com",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "三字母"
+    }, {
+        admin_uid: "12",
+        bid_price: "3344元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "ymmf.com",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "四声"
+    },
+
+    {
+        admin_uid: "13",
+        bid_price: "2137元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "ysss.cn",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "四字母"
+    },
+
+    {
+        admin_uid: "14",
+        bid_price: "875元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "76339.com",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "三声"
+    },
+
+    {
+        admin_uid: "15",
+        bid_price: "5432元",
+        buyer_frozen_price: "0",
+        buyer_uid: "0",
+        create_datetime: "2016-12-24 11:48:14",
+        domain: "87625.com",
+        domain_id: "9",
+        end: "2天22小时",
+        end_datetime: "2016-12-31 11:41:04",
+        frozen_price: "0",
+        meaning: "腾讯域名",
+        price: "0",
+        type: "三字母"
     }
 
 ]
-$(document).ready(function() {
-    render();
-})
+
 
 function render() {
 	for (var i = 0; i < data.length; i++) {
--- script/index.js
+++ script/index.js
@@ -1,23 +1,3 @@
-/*function loginHide() {
-    var userName = $("#username").val();
-    var userPass = $("#password").val();
-    if (!userName) {
-        alert("用户名不能为空");
-    } else if (!userPass) {
-        alert("密码不能为空");
-    } else {
-        $('#login').addClass('hide');
-    }
- }
-
- function loginShow() {
-    $("#password").val('');
-    $('#login').removeClass('hide');
- }
-
-   
-*/
-
 function login() {
     var userName = $("#username").val();
     var userPass = $("#password").val();
@@ -29,6 +9,7 @@ function login() {
         $('#login').toggle();
     }
 }
+
 function tab1() {
     $('.tab').removeClass('show');
     $('#tab1').addClass('show');
@@ -49,4 +30,16 @@ function tab4() {
     $('#tab4').addClass('show');
 }
 
+function myDropdown() {
+    $('.dropdown li').on('click', function() {
+        var text = $(this).text();
+        var parent = $(this).parents(".dropdown");
+        parent.find('.text').text(text);
+    })
+}
 
+
+$(document).ready(function() {
+    myDropdown();
+    render();
+})
